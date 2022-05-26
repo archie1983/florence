20473721e6a5f008ed909ec4fc336cbcae7b14a1
Arturs Elksnis <archie1983@hotmail.co.uk>
2022-05-26 18:18:41 +0100

    Adding a new move_base_demo launch script- fewer errors.

--- launch_gazebo.sh
+++ launch_gazebo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /mnt/storage/florence/florence_ws/env_setup.sh
+source env_setup.sh
 roslaunch husky_gazebo husky_playpen.launch
---
+++ launch_move_base_demo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source env_setup.sh
+roslaunch husky_navigation move_base_mapless_demo.launch
