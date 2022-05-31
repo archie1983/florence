9870028340fbe6cc872fbcce0433f271c1e58dfe
Arturs Elksnis <archie1983@hotmail.co.uk>
2022-05-31 22:53:41 +0100

    Adding camera

--- env_setup.sh
+++ env_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-export HUSKY_UST10_ENABLED=true
+#export HUSKY_UST10_ENABLED=true
 export HUSKY_LMS1XX_ENABLED=true
+#export HUSKY_REALSENSE_ENABLED=true
+#export HUSKY_URDF_EXTRAS="$(find husky_description)/urdf/cameras.urdf.xacro"
+export HUSKY_URDF_EXTRAS="cameras.urdf.xacro"
 source ../cartographer_ws/devel_isolated/setup.bash
 source devel/setup.bash
