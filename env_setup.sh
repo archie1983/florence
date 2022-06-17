746b7b3124be95c633d2cfe3a2bb0d721fde5c3e
Arturs Elksnis <archie1983@hotmail.co.uk>
2022-06-17 23:11:39 +0100

    Experiments with Lidar, cameras and ROS TCP endpoint

--- env_setup.sh
+++ env_setup.sh
@@ -4,5 +4,9 @@ export HUSKY_LMS1XX_ENABLED=true
 #export HUSKY_REALSENSE_ENABLED=true
 #export HUSKY_URDF_EXTRAS="$(find husky_description)/urdf/cameras.urdf.xacro"
 export HUSKY_URDF_EXTRAS="cameras.urdf.xacro"
+export RPLIDAR=true
+export USB_CAM=true
+export ROS_TCP_ENDPOINT_IPADDR="192.168.8.199"
+export ROS_TCP_ENDPOINT_PORT="10000"
 source ../cartographer_ws/devel_isolated/setup.bash
 source devel/setup.bash
