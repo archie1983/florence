5262477368f32af8c3ba257222ff29464b3eb537
Arturs Elksnis <archie1983@hotmail.co.uk>
2022-07-18 12:56:02 +0100

    Attempting to re-configure for RPliar instead of Hokuyo. Crashing for now.

--- env_setup.sh
+++ env_setup.sh
@@ -2,21 +2,21 @@
 #export HUSKY_LMS1XX_ENABLED=true
 #export HUSKY_REALSENSE_ENABLED=true
 #export HUSKY_URDF_EXTRAS="$(find husky_description)/urdf/cameras.urdf.xacro"
-#export RPLIDAR=false
+export RPLIDAR=true
 export USB_CAM=true
 #export SIM_CAM=true
 export HUSKY_URDF_EXTRAS="cameras.urdf.xacro"
-export ROS_TCP_ENDPOINT_IPADDR="192.168.8.199"
+export ROS_TCP_ENDPOINT_IPADDR="192.168.8.186"
 export ROS_TCP_ENDPOINT_PORT="10000"
-export HUSKY_UST10_ENABLED=true
-export HUSKY_UST10_IP="192.168.131.20"
-export HUSKY_UST10_SECONDARY_IP="192.168.131.21"
-export HUSKY_UST10_ENABLED=true
-export HUSKY_UST10_SECONDARY_ENABLED=true
+#export HUSKY_UST10_ENABLED=true
+#export HUSKY_UST10_IP="192.168.131.20"
+#export HUSKY_UST10_SECONDARY_IP="192.168.131.21"
+#export HUSKY_UST10_ENABLED=true
+#export HUSKY_UST10_SECONDARY_ENABLED=true
 export HUSKY_FRONT_BUMPER_EXTEND=false
 export HUSKY_REAR_BUMPER_EXTEND=false
-export HUSKY_UST10_XYZ="0.3812 0.0 -0.35635"
-export HUSKY_UST10_SECONDARY_XYZ="-0.4712 0.0 -0.35635"
+#export HUSKY_UST10_XYZ="0.3812 0.0 -0.35635"
+#export HUSKY_UST10_SECONDARY_XYZ="-0.4712 0.0 -0.35635"
 export HUSKY_LASER_TOPIC="/scan_filtered"
 source /home/jet/robotics/cartographer_ws/devel_isolated/setup.bash
 source /home/jet/robotics/florence_ws/devel/setup.bash
