745eb546971354e1a73f8678897a68deff6da5ba
Arturs Elksnis <archie1983@hotmail.co.uk>
2022-12-01 15:26:22 +0000

    Adding my own LiDAR based on LD06. Rviz still fails- can't find the stl.

--- env_setup.sh
+++ env_setup.sh
@@ -2,17 +2,23 @@
 #export HUSKY_LMS1XX_ENABLED=true
 #export HUSKY_REALSENSE_ENABLED=true
 #export HUSKY_URDF_EXTRAS="$(find husky_description)/urdf/cameras.urdf.xacro"
-export RPLIDAR=true
-export USB_CAM=true
+#export RPLIDAR=false
+export AELIDAR=true
+export USB_CAM=false
 #export SIM_CAM=true
-export HUSKY_URDF_EXTRAS="cameras.urdf.xacro"
+export HUSKY_URDF_EXTRAS="cameras_and_lidars.urdf.xacro"
 export ROS_TCP_ENDPOINT_IPADDR="192.168.8.186"
 export ROS_TCP_ENDPOINT_PORT="10000"
-#export HUSKY_UST10_ENABLED=true
+#export HUSKY_UST10_ENABLED=false
 #export HUSKY_UST10_IP="192.168.131.20"
 #export HUSKY_UST10_SECONDARY_IP="192.168.131.21"
-#export HUSKY_UST10_ENABLED=true
 #export HUSKY_UST10_SECONDARY_ENABLED=true
+
+export HUSKY_SENSOR_ARCH='1'
+export HUSKY_SENSOR_ARCH_HEIGHT='510' # or 300
+export HUSKY_SENSOR_ARCH_OFFSET='0 0 0'
+export HUSKY_SENSOR_ARCH_RPY='0 0 0'
+
 export HUSKY_FRONT_BUMPER_EXTEND=false
 export HUSKY_REAR_BUMPER_EXTEND=false
 #export HUSKY_UST10_XYZ="0.3812 0.0 -0.35635"
